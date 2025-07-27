f800076b4622e511ea590b54089d1125401f86b7
ke1yu <u0t0s0u0b0u@gmail.com>
2025-07-28 00:04:17 +0900

    commit

--- lingual.py
+++ lingual.py
@@ -23,7 +23,7 @@ def get_locale(language, key, *args):
       Str_Dict_Keys.HELP_LANG : '言語を切り替えます（管理者限定）。\nSwitch languages. Administrator permissons are required.'
     },
     Lang.EN : {
-      Str_Dict_Keys.DEFAULT : 'All VCs',
+      Str_Dict_Keys.ALL : 'All VCs',
       Str_Dict_Keys.DEFAULT : 'Unspecified VCs',
       Str_Dict_Keys.ALERT : '{} in \"{}\"',
       Str_Dict_Keys.ALERT_NAME_JOIN : '{} has joined \"{}\": {} in total',

9bcd213d4ba556610a6a165456aeeaac9350e48b
ke1yu <u0t0s0u0b0u@gmail.com>
2025-07-28 00:24:56 +0900

    commit

--- main.py
+++ main.py
@@ -170,12 +170,12 @@ async def send_here_command(interaction, vc: str):
         g[Db_Keys.ALERT_CHANNEL][Str_Dict_Keys.DEFAULT] = channel_id
         Database.update(g_id, Db_Keys.ALERT_CHANNEL, g[Db_Keys.ALERT_CHANNEL])
 
-        await interaction.response.send_message(get_locale(lang, Str_Dict_Keys.SEND_HERE, get_locale(lang, Str_Dict_Keys.DEFAULT, interaction.channel.name)))
+        await interaction.response.send_message(get_locale(lang, Str_Dict_Keys.SEND_HERE, get_locale(lang, Str_Dict_Keys.DEFAULT), interaction.channel.name))
       else:
         g[Db_Keys.ALERT_CHANNEL][vc] = channel_id
         Database.update(g_id, Db_Keys.ALERT_CHANNEL, g[Db_Keys.ALERT_CHANNEL])
 
-        await interaction.response.send_message(get_locale(lang, Str_Dict_Keys.SEND_HERE, get_locale(lang, vc, interaction.channel.name)))
+        await interaction.response.send_message(get_locale(lang, Str_Dict_Keys.SEND_HERE, interaction.guild.get_channel(int(channel_id)), interaction.channel.name))
 
     else:
       await interaction.response.send_message(get_locale(lang, Str_Dict_Keys.NO_PERMISSIONS), ephemeral=True)
